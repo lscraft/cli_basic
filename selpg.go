db4b650d4e8dbc85c090d9e0bcd9750a5630640c
lscraft <lvseqiji@vip.qq.com>
2017-10-16 20:33:50 +0800

    Update selpg.go

--- selpg.go
+++ selpg.go
@@ -9,63 +9,126 @@ import (
 	"os/exec"
 )
 
-func main() {
-	var s, e, l int
-	var d string
+var s, e, l int
+var d string
+var f bool
+
+func arginit() {
 	flag.IntVar(&s, "s", 1, "start page, default 1")
 	flag.IntVar(&e, "e", 1, "end page, deafault 1")
 	flag.IntVar(&l, "l", 72, "how many lines in single page, deafault 72")
 	flag.StringVar(&d, "d", "", "pipe or not, default nil")
+	flag.BoolVar(&f, "f", false, "search for the page break word")
 	flag.Parse()
+}
+
+func judge() {
+	if f == true && l != 72 {
+		io.WriteString(os.Stderr, "-f and -l should not appear together")
+		os.Exit(1)
+	}
+	if s < 1 || e < 1 || l < 1 {
+		io.WriteString(os.Stderr, "input integer please")
+		os.Exit(1)
+	}
+}
+func pipeToLp(cmdinfo string) {
+	cmdArg := "-d" + d
+	cmd := exec.Command("lp", cmdArg)
+	cmdin, cmderr := cmd.StdinPipe()
+	if cmderr != nil {
+		cmdin.Close()
+		os.Exit(1)
+	}
+	cmd.Start()
+	io.WriteString(cmdin, cmdinfo)
+	cmdin.Close()
+	cmd.Wait()
+}
+
+// function to deal with -f
+func splitWithF(fpointer io.Reader) string {
+	cmdinfo := ""
+	begin := s - 1
+	end := e
+	count := 1
+	reader := bufio.NewReader(fpointer)
+	for {
+		by, err := reader.ReadByte()
+		if err == io.EOF {
+			break
+		}
+		if string(by) == "\f" {
+			count = count + 1
+		} else if count > begin && count <= end {
+			if d == "" {
+				fmt.Printf("%s", string(by))
+			} else {
+				cmdinfo = cmdinfo + string(by)
+			}
+		}
+	}
+	if d == "" {
+		fmt.Printf("\n")
+	} else {
+		cmdinfo = cmdinfo + "\n"
+	}
+	return cmdinfo
+}
+
+//normal scan function
+func splitWithoutF(fpointer io.Reader) string {
 	begin := (s - 1) * l
 	end := e * l
 	count := 1
 	cmdinfo := ""
-	//without filename ,read info from standard input
-	if len(flag.Args()) == 0 {
-		scanner := bufio.NewScanner(os.Stdin)
-		for scanner.Scan() {
-			line := scanner.Text()
-			if count > begin && count <= end {
-				if d == "" {
-					fmt.Println(line)
-				} else {
-					cmdinfo = cmdinfo + line + "\n"
-				}
+	scanner := bufio.NewScanner(fpointer)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if count > begin && count <= end {
+			if d == "" {
+				fmt.Println(line)
+			} else {
+				cmdinfo = cmdinfo + line + "\n"
 			}
 		}
-		//with filename, read info from file
+		count = count + 1
+	}
+	return cmdinfo
+}
+func processInfo(fp io.Reader) string {
+
+	if f == true {
+		return splitWithF(fp)
 	} else {
-		fileName := flag.Args()[0]
-		f, err := os.Open(fileName)
+		return splitWithoutF(fp)
+	}
+}
+func getFilePointer() io.Reader {
+	var fp io.Reader
+	if len(flag.Args()) == 0 {
+		fp = os.Stdin
+	} else {
+		file, err := os.Open(flag.Args()[0])
+		fp = bufio.NewReader(file)
 		if err != nil {
-			f.Close()
+			file.Close()
 			os.Exit(1)
-		}
-		scanner := bufio.NewScanner(f)
-		for scanner.Scan() {
-			line := scanner.Text()
-			if count > begin && count <= end {
-				if d == "" {
-					fmt.Println(line)
-				} else {
-					cmdinfo = cmdinfo + line + "\n"
-				}
-			}
 		}
 	}
+	return fp
+}
+func main() {
+	arginit()
+
+	judge()
+
+	fp := getFilePointer()
+
+	cmdinfo := processInfo(fp)
+
 	//with -d option ,it will pipe info to command "lp -d***"
 	if d != "" {
-		cmdArg := "-d" + d
-		cmd := exec.Command("lp", cmdArg)
-		cmdin, cmderr := cmd.StdinPipe()
-		if cmderr != nil {
-			cmdin.Close()
-			os.Exit(1)
-		}
-		cmd.Start()
-		io.WriteString(cmdin, cmdinfo)
-		cmdin.Close()
-		cmd.Wait()
+		pipeToLp(cmdinfo)
 	}
 }
